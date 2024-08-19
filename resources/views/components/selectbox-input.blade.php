a1e3cff5608d4f52a5a4dee044c93d49432b2659
USER-PC\User <soyamamasashi@gmail.com>
2024-08-19 14:57:29 +0900

    'xxx'

--- resources/views/components/selectbox-input.blade.php
+++ resources/views/components/selectbox-input.blade.php
@@ -6,6 +6,6 @@
 
 <select name="{{$name}}" {!! $attributes->merge(['class' => '']) !!}>
 	@foreach($options as $option_name=>$value)
-		<option value=""{{ in_array((string)$value, array_map('strval', (array)old(preg_replace("/\[\s*\]/", '', $name), $default)), true) ? ' selected' : '' }}>{{$option_name}}</option>
+		<option value=""{{ in_array((string)$value, array_map('strval', (array)old(str_replace(['[',']'],['.',''],preg_replace("/\[\s*\]/", '', $name)), $default)), true) ? ' selected' : '' }}>{{$option_name}}</option>
 	@endforeach
 </select>
